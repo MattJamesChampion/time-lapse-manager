7721e3942a433998a6b44839048b1c20db98b861
Matthew Champion <mattjameschampion@gmail.com>
2018-02-24 18:12:30 +0000

    Add basic implementation of mock_camera

---
+++ tests/mocks/mock_camera.py
@@ -0,0 +1,33 @@
+"""A mock implementation of a basic camera."""
+
+import os
+import sys
+from abstract_camera import AbstractCamera
+
+
+class MockCamera(AbstractCamera):
+    """A class representing a mock camera."""
+
+    def __init__(self):
+        """Initialise the mock camera."""
+        self._captured_images = []
+
+    def capture_image(self, image_path, filename, overwrite=False):
+        """Capture an image and store it in the specified location.
+
+        Args:
+            image_path: The full path that the image will be stored to.
+            filename: The file name that the image will be stored as
+                (including extension).
+            overwrite: Specifies whether or not to overwrite a file if it
+                already exists in the specified image_path with the specified
+                filename.
+        """
+        self._captured_images.append(os.path.join(image_path, filename))
+
+    def get_captured_images(self):
+        """Return a list of filepaths of the images captured.
+
+        Returns: A list of filepaths of the images captured.
+        """
+        return self._captured_images
