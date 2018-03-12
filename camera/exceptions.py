75bad2dd02ea5a6f35f1d2ecf094246a807ec90a
Matthew Champion <mattjameschampion@gmail.com>
2018-03-12 20:38:07 +0000

    Add the CameraConnectionError exception

--- camera/exceptions.py
+++ camera/exceptions.py
@@ -1,6 +1,11 @@
 """A collection of custom Exceptions specifically pertaining to cameras."""
 
 
+class CameraConnectionError(Exception):
+    """Camera could not be contacted."""
+    pass
+
+
 class CameraCaptureError(Exception):
     """Camera could not capture the image."""
     pass
